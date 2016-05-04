40adde569c08c3d35c1ecca67ea39cd92435ec8b
tom1230123 <853838646@qq.com>
2016-05-05 00:29:56 +0800

    testCommit

---
+++ Tom_sqlite3Demo/Tom_sqlite3Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tom_sqlite3Demo
+//
+//  Created by Tom-MAC on 16/5/5.
+//  Copyright © 2016年 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Tom_sqlite3Demo/Tom_sqlite3DemoTests/Tom_sqlite3DemoTests.m
@@ -0,0 +1,39 @@
+//
+//  Tom_sqlite3DemoTests.m
+//  Tom_sqlite3DemoTests
+//
+//  Created by Tom-MAC on 16/5/5.
+//  Copyright © 2016年 developer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tom_sqlite3DemoTests : XCTestCase
+
+@end
+
+@implementation Tom_sqlite3DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
